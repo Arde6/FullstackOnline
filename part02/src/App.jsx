9697a5e1c9eff6ab17c05a18b2a5d64f90109ef9
arde <artturi.juurenheimo@edu.turkuamk.fi>
2026-01-19 12:02:15 +0200

    Switching os

--- part02/src/App.jsx
+++ part02/src/App.jsx
@@ -5,6 +5,7 @@ import Courses from './components/Courses'
 import Name from './components/Name'
 import peopleService from './services/people'
 import Notification from './components/Notification'
+import countryService from './services/countries'
 
 /**
  * -------- Example ---------
@@ -141,6 +142,188 @@ import Notification from './components/Notification'
 
 /**
  * --------- Part b ---------
+ * --------- Part c ---------
+ * --------- Part d ---------
+ * --------- Part e ---------
+ */
+
+// const Filter = ({ text, filter, handleFilterChange }) => {
+
+//   return (
+//     <div>
+//       <form>
+//         {text}
+//         <input
+//           value={filter}
+//           onChange={handleFilterChange}
+//         />
+//       </form>
+//     </div>
+//   )
+// }
+
+// const PersonForm = ({ addName, newName, handleNameChange, newNum, handleNumChange }) => {
+
+//   return (
+//     <form onSubmit={addName}>
+//       <div>
+//         name: 
+//         <input 
+//           value={newName}
+//           onChange={handleNameChange}
+//         />
+//         <br/>
+//         number:
+//         <input 
+//           value={newNum}
+//           onChange={handleNumChange}
+//         />
+//         <br/>
+//         <button type="submit">add</button>
+//       </div>
+//     </form>
+//   )
+// }
+
+// const Persons = ({ persons, filter, deleteName }) => {
+
+//   const namesToShow = persons.filter(person =>
+//     person.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <ul>
+//       {namesToShow.map(person => 
+//         <Name key={person.name} person={person} deleteName={deleteName} />
+//       )}
+//     </ul>
+//   )
+// }
+
+// const App = () => {
+
+//   const [persons, setPersons] = useState([])
+//   const [newName, setNewName] = useState('')
+//   const [newNum, setNewNum] = useState('')
+//   const [filter, setFilter] = useState('')
+
+//   const [errorMessage, setErrorMessage] = useState(null)
+//   const [infoMessage, setInfoMessage] = useState(null)
+
+//   useEffect(() => {
+//     peopleService
+//       .getAll()
+//       .then(response => {
+//         setPersons(response.data)
+//       })  
+//   }, [])
+
+//   const addName = (event) => {
+//     event.preventDefault()
+    
+//     const errorMessage = `${newName} is already added to phonebook, replace the old number with a new one?`
+
+//     const nameExists = persons.some(person => person.name === newName)
+
+//     const nameObject = {
+//       name: newName,
+//       number: newNum,
+//     }
+
+//     if (nameExists) {
+//       const personToUpdate = persons.find(person => person.name === newName)
+//       const id = personToUpdate.id
+//       if (window.confirm(errorMessage)) {
+//         peopleService
+//           .update(id, nameObject)
+//           .then((returnedPerson) => {
+//             setPersons(persons.map((person) => (person.id !== id ? person : returnedPerson)))
+//             setInfoMessage(nameObject.name + ' modified succesfully')
+//             setTimeout(() => {
+//               setInfoMessage(null)
+//             }, 5000)
+//           })
+//           .catch((error) => {
+//             setErrorMessage(
+//               'Person doesn\'t exist anymore'
+//             )
+//             setTimeout(() => {
+//               setErrorMessage(null)
+//             }, 5000)
+//           })
+//       }
+//     } else if (newNum === '' || newName === ''){
+//         setErrorMessage(
+//           'Name or number is empty'
+//         )
+//         setTimeout(() => {
+//           setErrorMessage(null)
+//         }, 5000)
+//     } else {
+//         peopleService
+//           .create(nameObject)
+//           .then(response => {
+//             setPersons(persons.concat(response.data))
+//             setNewName('')
+//             setNewNum('')
+//             setInfoMessage(nameObject.name + ' added succesfully')
+//             setTimeout(() => {
+//               setInfoMessage(null)
+//             }, 5000);
+//           })
+//     }
+//   }
+
+//   const deleteName = (id) => {
+//     if (window.confirm('Delete ' + persons.find(person => person.id === id).name + '?')) {
+//       peopleService
+//         .deleteName(id)
+//         .then(() => {
+//           setPersons(persons.filter(person => person.id !== id));
+//         })
+//     }
+//   }
+
+//   const handleNameChange = (event) => {
+//     setNewName(event.target.value)
+//   }
+
+//   const handleNumChange = (event) => {
+//     setNewNum(event.target.value)
+//   }
+
+//   const handleFilterChange = (event) => {
+//     setFilter(event.target.value)
+//   }
+
+//   return (
+//     <div>
+
+//       <h1>Phonebook</h1>
+
+//       <Notification message={errorMessage} type="error" />
+//       <Notification message={infoMessage} type="info" />
+
+//       <Filter text={"search: "} filter={filter} handleFilterChange={handleFilterChange} />
+
+//       <h2>Add new</h2>
+
+//       <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNum={newNum} handleNumChange={handleNumChange} />
+
+//       <h2>Numbers</h2>
+
+//       <Persons persons={persons} filter={filter} deleteName={deleteName} />
+
+//     </div>
+//   )
+// }
+
+/**
+ * --------- Part b ---------
+ */
+
+/**
+ * --------- Part e ---------
  */
 
 const Filter = ({ text, filter, handleFilterChange }) => {
@@ -158,39 +341,11 @@ const Filter = ({ text, filter, handleFilterChange }) => {
   )
 }
 
-const PersonForm = ({ addName, newName, handleNameChange, newNum, handleNumChange }) => {
-
-  return (
-    <form onSubmit={addName}>
-      <div>
-        name: 
-        <input 
-          value={newName}
-          onChange={handleNameChange}
-        />
-        <br/>
-        number:
-        <input 
-          value={newNum}
-          onChange={handleNumChange}
-        />
-        <br/>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
-
-const Persons = ({ persons, filter, deleteName }) => {
-
-  const namesToShow = persons.filter(person =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
+const AllCountries = ({ countriesToShow }) => {
   return (
     <ul>
-      {namesToShow.map(person => 
-        <Name key={person.name} person={person} deleteName={deleteName} />
+      {countriesToShow.map(country => 
+        <li key={country.name.common}>{country.name.common}</li>
       )}
     </ul>
   )
@@ -198,124 +353,51 @@ const Persons = ({ persons, filter, deleteName }) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNum, setNewNum] = useState('')
+  const [countries, setCountries] = useState(null)
   const [filter, setFilter] = useState('')
 
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [infoMessage, setInfoMessage] = useState(null)
-
   useEffect(() => {
-    peopleService
+    countryService
       .getAll()
       .then(response => {
-        setPersons(response.data)
+        setCountries(response.data)
       })  
-  }, [])
+  }, [countries])
 
-  const addName = (event) => {
-    event.preventDefault()
-    
-    const errorMessage = `${newName} is already added to phonebook, replace the old number with a new one?`
-
-    const nameExists = persons.some(person => person.name === newName)
-
-    const nameObject = {
-      name: newName,
-      number: newNum,
-    }
-
-    if (nameExists) {
-      const personToUpdate = persons.find(person => person.name === newName)
-      const id = personToUpdate.id
-      if (window.confirm(errorMessage)) {
-        peopleService
-          .update(id, nameObject)
-          .then((returnedPerson) => {
-            setPersons(persons.map((person) => (person.id !== id ? person : returnedPerson)))
-            setInfoMessage(nameObject.name + ' modified succesfully')
-            setTimeout(() => {
-              setInfoMessage(null)
-            }, 5000)
-          })
-          .catch((error) => {
-            setErrorMessage(
-              'Person doesn\'t exist anymore'
-            )
-            setTimeout(() => {
-              setErrorMessage(null)
-            }, 5000)
-          })
-      }
-    } else if (newNum === '' || newName === ''){
-        setErrorMessage(
-          'Name or number is empty'
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-    } else {
-        peopleService
-          .create(nameObject)
-          .then(response => {
-            setPersons(persons.concat(response.data))
-            setNewName('')
-            setNewNum('')
-            setInfoMessage(nameObject.name + ' added succesfully')
-            setTimeout(() => {
-              setInfoMessage(null)
-            }, 5000);
-          })
-    }
+  if(countries === null) {
+    return null
   }
 
-  const deleteName = (id) => {
-    if (window.confirm('Delete ' + persons.find(person => person.id === id).name + '?')) {
-      peopleService
-        .deleteName(id)
-        .then(() => {
-          setPersons(persons.filter(person => person.id !== id));
-        })
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumChange = (event) => {
-    setNewNum(event.target.value)
-  }
+  const countriesToShow = countries.filter(country =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
 
+  if (countriesToShow.lenght === 1) {
+    return (
+      <div>
+        <Filter text={"search: "} filter={filter} handleFilterChange={handleFilterChange} />
+      </div>
+    )
+  }
+
   return (
     <div>
-
-      <h1>Phonebook</h1>
-
-      <Notification message={errorMessage} type="error" />
-      <Notification message={infoMessage} type="info" />
-
       <Filter text={"search: "} filter={filter} handleFilterChange={handleFilterChange} />
 
-      <h2>Add new</h2>
-
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNum={newNum} handleNumChange={handleNumChange} />
-
-      <h2>Numbers</h2>
-
-      <Persons persons={persons} filter={filter} deleteName={deleteName} />
-
+      <AllCountries countriesToShow={countriesToShow} />
     </div>
   )
 }
 
 /**
- * --------- Part b ---------
+ *   const namesToShow = country.filter(country =>
+    country.name.toLowerCase().includes(filter.toLowerCase())
+  );
+ * --------- Part e ---------
  */
 
 export default App
